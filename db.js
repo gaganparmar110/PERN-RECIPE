a0fd679ccabef40574b46c6fe91e4e499b546ecc
Gagan Parmar <gaganparmar110@gmail.com>
2020-11-01 22:41:41 +0530

    modified db

--- db.js
+++ db.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 //     port : process.env.PG_PORT,
 //     database : process.env.PG_DATABASE
 // };
-const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABSE}`;
+const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
 const proConfig =  process.env.DATABASE_URL;   //heroku addon
 
